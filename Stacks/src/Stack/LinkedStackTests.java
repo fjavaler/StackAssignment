1db9dde8199bb28a65788daf3cc516b1bbb24f24
fjavaler <conscientiadivinus@gmail.com>
2017-04-10 23:41:08 -0600

    Created LinkedStackTests.java

---
+++ Stacks/src/Stack/LinkedStackTests.java
@@ -0,0 +1,52 @@
+package Stack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LinkedStackTests
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@Test
+	public void testLinkedStack()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPeek()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPop()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPush()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSize()
+	{
+		fail("Not yet implemented");
+	}
+
+}
